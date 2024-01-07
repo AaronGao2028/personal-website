8f53ba1964e9de79e2e73ea43b51af9a89505a5a
AaronGao2028 <a43gao@uwaterloo.ca>
2024-01-06 21:24:55 -0500

    Added animated text in homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,27 +1,35 @@
 import { HStack, Heading, Image, Text, VStack } from "@chakra-ui/react";
 import profile from "../../public/images/profile.jpg";
+import { Cursor, useTypewriter } from "react-simple-typewriter";
 
 const Home = () => {
+  const [intro] = useTypewriter({
+    words: ["Hi, My name is Aaron!"],
+    loop: 1,
+  });
+  const [text] = useTypewriter({
+    words: ["Software Developer", "Competitive Programmer", "Bodybuilder"],
+    loop: 10,
+  });
+
   return (
     <>
-      <VStack height="100vh">
-        <Heading alignItems={"center"}>Hi, My name is Aaron</Heading>
-        <HStack>
-          <Image borderRadius={50} src={profile} height={"300px"} />;
-          <VStack textAlign={"left"} fontSize="20px">
-            <Text>
-              I am a first year student at the University of Waterloo pursuing a
-              degree in Computer Science. I am currently in my 1B term and
-              seeking summer internships.
-            </Text>
-            <Text>
-              I enjoy creating full stack web applications using a variety of
-              libraries, frameworks, and developer tools.
-            </Text>
-            <Text>
-              When I am not programming, I enjoy weightlifting, exploring the
-              stock market, and reading history!
-            </Text>
+      <VStack minHeight="100vh">
+        <HStack marginTop={100}>
+          <Image
+            marginLeft={10}
+            borderRadius={50}
+            src={profile}
+            height={"300px"}
+          />
+          <VStack marginLeft={50}>
+            <Heading fontSize="40px" marginTop={20} alignItems={"center"}>
+              {intro}
+            </Heading>
+            <Heading fontSize="30px" alignItems={"center"}>
+              I am a {text}
+              <Cursor cursorStyle="|" />
+            </Heading>
           </VStack>
         </HStack>
       </VStack>
